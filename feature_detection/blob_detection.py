58d1fc939a8c30139165f6d6480e44b9350f3048
sean2285 <133795823+sean2285@users.noreply.github.com>
2025-10-28 19:16:20 -0400

    new

--- feature_detection/blob_detection.py
+++ feature_detection/blob_detection.py
@@ -10,7 +10,7 @@ sift = cv2.SIFT_create(
     contrastThreshold=0.075,   
     edgeThreshold=2.5,        
     sigma=3,               
-    nOctaveLayers=5         
+    nOctaveLayers=4         
 )
 
 #Tunable parameters
