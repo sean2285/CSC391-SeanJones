3e6edb109d249b9e0d6fcbd6d1a2708ecf81cc53
sean2285 <133795823+sean2285@users.noreply.github.com>
2025-10-28 15:09:28 -0400

    new

--- feature_detection/blob_detection.py
+++ feature_detection/blob_detection.py
@@ -7,7 +7,7 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 #Initizalize SIFT with custom parameters
 sift = cv2.SIFT_create(
-    contrastThreshold=0.03,   
+    contrastThreshold=0.0275,   
     edgeThreshold=2.5,        
     sigma=2,               
     nOctaveLayers=4         
